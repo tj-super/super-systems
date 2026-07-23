0e3de7959cceac5609aaf48e52c4ed71323ce978
TJ Super <tj.super@proton.me>
2026-07-23 11:02:29 -0500

    fixed pubkey outputs

--- flake.nix
+++ flake.nix
@@ -45,8 +45,8 @@
     in
     {
       pubKeys.ssh = {
-        users.super = import ./keys/ssh/user.pub;
-        hosts.super-laptop = import ./keys/ssh/host.pub;
+        users.super = builtins.readFile ./keys/ssh/user.pub;
+        hosts.super-laptop = builtins.readFile ./keys/ssh/host.pub;
       };
 
       devShells.${system}.default = import ./shell/dev-shell.nix {
