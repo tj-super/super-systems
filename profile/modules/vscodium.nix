e8374e7bc37732d0f7180b3933f87386e11ab721
TJ Super <tj.super@proton.me>
2026-08-05 05:10:01 -0500

    moved nix ide stuff out of vscodium.nix

--- profile/modules/vscodium.nix
+++ profile/modules/vscodium.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nixd
-    nixfmt
-  ];
-
   programs.vscodium = {
     enable = true;
 
@@ -13,26 +8,11 @@
       extensions = with pkgs.vscode-extensions; [
         bierner.markdown-mermaid
         bierner.markdown-preview-github-styles
-        jnoortheen.nix-ide
         mkhl.direnv
       ];
       userSettings = {
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
-        "nix.serverSettings" = {
-          "nixd" = {
-            "formatting" = {
-              "command" = [
-                "nixfmt"
-              ];
-            };
-          };
-        };
-        "[nix]" = {
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
-          "editor.formatOnSave" = true;
-          "editor.tabSize" = 2;
-          "editor.insertSpaces" = true;
+        "terminal.integrated.env.linux" = {
+          "DIRENV_LOG_FORMAT" = "";
         };
       };
     };
